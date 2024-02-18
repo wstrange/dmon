45b154cd45a8d9b67ea4ca7c2de98b283b9a3990
warren <warren.strange@gmail.com>
2024-02-17 18:09:24 -0700

    readme

--- linux_proc/test/process_test.dart
+++ linux_proc/test/process_test.dart
@@ -6,7 +6,7 @@ main() async {
     var stopwatch = Stopwatch();
 
     // see how long it takes for multiple iterations..
-    for (int i = 0; i < 20; ++i) {
+    for (int i = 0; i < 10; ++i) {
       stopwatch.reset();
       stopwatch.start();
       var m = await Process.getAllProcesses();
